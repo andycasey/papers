923c19bef2db7d8bd33069482bba447213a313c8
andycasey <andycasey@gmail.com>
2013-01-13 14:46:15 -0800

    Added Mg-Al plot and code

--- 2012-aqs/figures/plot-mg-al.py
+++ 2012-aqs/figures/plot-mg-al.py
@@ -1,7 +1,125 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Get Na and O from files:
+# Get data from Reddy et al 2003
+reddy_2003 = np.loadtxt('Reddy_disk_2003.data', delimiter='|', usecols=(18, 19), dtype=str)
+
+Reddy_2003_Mg_Fe = []
+Reddy_2003_Al_Fe = []
+
+for mg, al in reddy_2003:
+	mg = mg.strip()
+	al = al.strip()
+
+	if len(mg) == 0:
+		mg = np.nan
+
+	else:
+		mg = float(mg)
+
+	if len(al) == 0:
+		al = np.nan
+
+	else:
+		al = float(al)
+
+	Reddy_2003_Al_Fe.append(al)
+	Reddy_2003_Mg_Fe.append(mg)
+
+
+Fulbright_2000_Al_Fe = []
+Fulbright_2000_Mg_Fe = []
+
+with open('Fulbright_2000.data', 'r') as fp:
+	data = fp.readlines()
+
+	for line in data:
+		if line.startswith('#'): continue
+
+		mg = line[26:31].strip()
+		al = line[32:37].strip()
+
+		if len(mg) == 0:
+			mg = np.nan
+
+		else:
+			mg = float(mg)
+
+		if len(al) == 0:
+			al = np.nan
+
+		else:
+			al = float(al)
+
+		Fulbright_2000_Mg_Fe.append(mg)
+		Fulbright_2000_Al_Fe.append(al)
+
+
+# Get data from Reddy et al 2005
+reddy_data = np.loadtxt('Reddy_thick_disk_2005.data', delimiter='|', usecols=(1, 18, 22, 23), dtype=str)
+
+Reddy_ThickDisk_Mg_Fe = []
+Reddy_ThickDisk_Al_Fe = []
+Reddy_ThinDisk_Mg_Fe = []
+Reddy_ThinDisk_Al_Fe = []
+Reddy_ThickThinDisk_Mg_Fe = []
+Reddy_ThickThinDisk_Al_Fe = []
+Reddy_Halo_Mg_Fe = []
+Reddy_Halo_Al_Fe = []
+Reddy_ThickDisk_Halo_Mg_Fe = []
+Reddy_ThickDisk_Halo_Al_Fe = []
+
+for classification, fe_h, mg_h, al_h in reddy_data:
+	
+	classification, fe_h, mg_h, al_h = [var.strip() for var in (classification, fe_h, mg_h, al_h)]
+
+
+	al_fe, mg_fe = np.nan, np.nan
+
+	if len(fe_h) == 0:
+		fe_h, mg_h, al_h = [np.nan] * 3
+
+	else:
+
+		if len(mg_h) == 0:
+			mg_h = np.nan
+
+		else:
+			mg_fe = float(mg_h) - float(fe_h)
+
+
+		if len(al_h) == 0:
+			al_h = np.nan
+
+		else:
+			al_fe = float(al_h) - float(fe_h)
+
+
+	if classification == 'Thick disc':
+		Reddy_ThickDisk_Al_Fe.append(al_fe)
+		Reddy_ThickDisk_Mg_Fe.append(mg_fe)
+
+	elif classification == 'Thin disc':
+		Reddy_ThinDisk_Al_Fe.append(al_fe)
+		Reddy_ThinDisk_Mg_Fe.append(mg_fe)
+
+	elif classification == 'Halo':
+		Reddy_Halo_Al_Fe.append(al_fe)
+		Reddy_Halo_Mg_Fe.append(mg_fe)
+
+	elif classification == 'Thin/thick disc':
+		Reddy_ThickThinDisk_Al_Fe.append(al_fe)
+		Reddy_ThickThinDisk_Mg_Fe.append(mg_fe)
+
+	elif classification == 'Thick disc/halo':
+		Reddy_ThickDisk_Halo_Al_Fe.append(al_fe)
+		Reddy_ThickDisk_Halo_Mg_Fe.append(mg_fe)
+
+
+
+Reddy_ThickDisk_Mg_Fe, Reddy_ThickDisk_Al_Fe, Reddy_ThinDisk_Mg_Fe, Reddy_ThinDisk_Al_Fe, Reddy_Halo_Mg_Fe, Reddy_Halo_Al_Fe, Reddy_ThickThinDisk_Mg_Fe, Reddy_ThickThinDisk_Al_Fe, Reddy_ThickDisk_Halo_Mg_Fe, Reddy_ThickDisk_Halo_Al_Fe = [np.array(item) for item in (Reddy_ThickDisk_Mg_Fe, Reddy_ThickDisk_Al_Fe, Reddy_ThinDisk_Mg_Fe, Reddy_ThinDisk_Al_Fe, Reddy_Halo_Mg_Fe, Reddy_Halo_Al_Fe, Reddy_ThickThinDisk_Mg_Fe, Reddy_ThickThinDisk_Al_Fe, Reddy_ThickDisk_Halo_Mg_Fe, Reddy_ThickDisk_Halo_Al_Fe)]
+
+
 data_files = ['c2225316-14437_abundances.data', 'c2306265-085103_abundances.data', 'j221821-183424_abundances.data', 'j223504-152834_abundances.data', 'j223811-104126_abundances.data']
 
 Mg_Fe = []
@@ -44,6 +162,18 @@ m, c = np.linalg.lstsq(A, Al_Fe)[0]
 fig = plt.figure()
 
 ax = fig.add_subplot(111)
+
+# Draw reddy
+ax.scatter(Reddy_ThickDisk_Mg_Fe, Reddy_ThickDisk_Al_Fe, marker='o', edgecolor='r', facecolor='none')
+ax.scatter(Reddy_ThinDisk_Mg_Fe, Reddy_ThinDisk_Al_Fe, marker='o', facecolor='none', edgecolor='g')
+ax.scatter(Reddy_Halo_Mg_Fe, Reddy_Halo_Al_Fe, marker='o', facecolor='none', edgecolor='b')
+
+ax.scatter(Reddy_ThickThinDisk_Mg_Fe, Reddy_ThickThinDisk_Al_Fe, marker='o', facecolor='none', edgecolor='m')
+ax.scatter(Reddy_ThickDisk_Halo_Mg_Fe, Reddy_ThickDisk_Halo_Al_Fe, marker='o', facecolor='none', edgecolor='orange')
+
+#ax.scatter(Reddy_2003_Mg_Fe, Reddy_2003_Al_Fe, marker='o', facecolor='none', edgecolor='#666666')
+ax.scatter(Fulbright_2000_Mg_Fe, Fulbright_2000_Al_Fe, marker='o', facecolor='none', edgecolor='k')
+
 ax.scatter(Mg_Fe, Al_Fe, color='k')
 ax.errorbar(Mg_Fe, Al_Fe, xerr=e_Mg_Fe, yerr=e_Al_Fe, fmt=None, ecolor='k')
 
@@ -54,8 +184,8 @@ ax.plot(x, m * x + c, 'k-')
 ax.set_xlabel('[Mg/Fe]')
 ax.set_ylabel('[Al/Fe]')
 
-ax.set_xlim(-0.5, 1.0)
-ax.set_ylim(-0.5, 1.5)
+#ax.set_xlim(-0.5, 1.0)
+#ax.set_ylim(-0.5, 1.5)
 
 plt.savefig('aquarius-mg-al.eps')
 plt.savefig('aquarius-mg-al.pdf')
